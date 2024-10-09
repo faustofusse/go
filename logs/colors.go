1ff1a297458363b102ff67cd9331ca2ae362fdfb
Fausto Fusse <faustofusse@gmail.com>
2024-10-09 18:13:35 -0300

    feat: text logs

--- logs/colors.go
+++ logs/colors.go
@@ -1,13 +1,43 @@
 package logs
 
-// ANSI color codes
-const (
-    Reset  = "\033[0m"
-    Red    = "\033[31m"
-    Green  = "\033[32m"
-    Yellow = "\033[33m"
-    Blue   = "\033[34m"
-    Purple = "\033[35m"
-    Cyan   = "\033[36m"
-    Gray   = "\033[90m"
+import (
+	"fmt"
+	"strconv"
 )
+
+// // ANSI color codes
+// const (
+//     Reset  = "\033[0m"
+//     Red    = "\033[31m"
+//     Green  = "\033[32m"
+//     Yellow = "\033[33m"
+//     Blue   = "\033[34m"
+//     Purple = "\033[35m"
+//     Cyan   = "\033[36m"
+//     Gray   = "\033[90m"
+// )
+
+const (
+	reset = "\033[0m"
+
+	black        = 30
+	red          = 31
+	green        = 32
+	yellow       = 33
+	blue         = 34
+	magenta      = 35
+	cyan         = 36
+	lightGray    = 37
+	darkGray     = 90
+	lightRed     = 91
+	lightGreen   = 92
+	lightYellow  = 93
+	lightBlue    = 94
+	lightMagenta = 95
+	lightCyan    = 96
+	white        = 97
+)
+
+func colorize(colorCode int, v string) string {
+	return fmt.Sprintf("\033[%sm%s%s", strconv.Itoa(colorCode), v, reset)
+}
--- logs/text.go
+++ logs/text.go
@@ -1,15 +1,106 @@
 package logs
 
 import (
+	"bytes"
+	"context"
+	"fmt"
 	"log/slog"
 	"os"
+	"sync"
 )
 
+const timeFormat = "15:04:02"
+
 func Text() *slog.Logger {
-    return slog.New(
-        slog.NewTextHandler(
-            os.Stdout,
-            &slog.HandlerOptions{ AddSource: false },
-        ),
+    options := slog.HandlerOptions{}
+    options.Level = slog.LevelDebug
+    options.AddSource = false
+    options.ReplaceAttr = func(groups []string, a slog.Attr) slog.Attr {
+        return slog.Any("", nil)
+    }
+    handler := TextHandler{
+        buffer: new(bytes.Buffer),
+        mutex: new(sync.Mutex),
+        wrapped: slog.NewTextHandler(os.Stdout, &options),
+    }
+    return slog.New(handler)
+}
+
+type TextHandler struct {
+	wrapped slog.Handler
+	buffer *bytes.Buffer
+	mutex *sync.Mutex
+}
+
+func (h TextHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.wrapped.Enabled(ctx, level)
+}
+
+func (h TextHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &TextHandler{ wrapped: h.wrapped.WithAttrs(attrs), buffer: h.buffer, mutex: h.mutex }
+}
+
+func (h TextHandler) WithGroup(name string) slog.Handler {
+	return &TextHandler{ wrapped: h.wrapped.WithGroup(name), buffer: h.buffer, mutex: h.mutex }
+}
+
+func levelString(level slog.Level) string {
+    switch level {
+        case slog.LevelDebug: return "debug"
+        case slog.LevelInfo: return "info "
+        case slog.LevelWarn: return "warn "
+        case slog.LevelError: return "error"
+        default: return "???"
+    }
+}
+
+func levelColor(level slog.Level) int {
+    switch level {
+        case slog.LevelDebug: return darkGray
+        case slog.LevelInfo: return cyan
+        case slog.LevelWarn: return lightYellow
+        case slog.LevelError: return lightRed
+        default: return white
+    }
+}
+
+func rightPad(str string, width int) string {
+    padded := str
+    for len(padded) < width {
+        padded += " "
+    }
+    return padded
+}
+
+func (h TextHandler) Handle(ctx context.Context, r slog.Record) error {
+    service := ""
+    attrs := ""
+
+    r.Attrs(func(a slog.Attr) bool {
+        if a.Key == "service" {
+            service = a.Value.String()
+        } else {
+            attrs += " " + colorize(lightGray, a.Key) + colorize(lightGray, "=") + colorize(white, a.Value.String())
+        }
+        return true
+    })
+
+    fmt.Print(
+        colorize(lightGray, r.Time.Format(timeFormat)),
+        " ",
+        colorize(levelColor(r.Level), levelString(r.Level)),
+        " ",
     )
+
+    if service != "" {
+        fmt.Printf("%-15s", colorize(lightGray, service))
+    }
+
+    fmt.Print(
+        colorize(levelColor(r.Level), " │ "),
+    )
+
+    fmt.Print(colorize(white, r.Message), attrs, "\n")
+
+    return nil
 }
